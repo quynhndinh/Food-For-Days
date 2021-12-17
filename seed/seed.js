52457077eca2425e8770e29b0fc002365607984f
QuynhDinh <quynhndinh96@gmail.com>
2021-12-17 12:24:41 -0500

    seed js file

---
+++ seed/seed.js
@@ -0,0 +1,23 @@
+const sequelize = require("../config/connection");
+const { User, Recipe, UserRecipe } = require("../models");
+
+const UserSeedData = require("./userData.json");
+const RecipesSeedData = require("./recipesData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(UserSeedData);
+  const recipes = await Recipe.bulkCreate(RecipesSeedData);
+
+  for (let i = 0; i < 10; i++) {
+    await UserRecipe.create({
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      recipe_id: recipes[Math.floor(Math.random() * recipes.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
